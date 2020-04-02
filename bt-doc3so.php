e8cfce1d2357adc3e335ab70b3b2d8efba9ee04b
TranTam-C0220H2 <tam.tvt.ptit@gmail.com>
2020-04-02 21:13:33 +0700

    add bt-doc3so

---
+++ bt-doc3so.php
@@ -0,0 +1,104 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<form method="get">
+    <input type="text" name="isNumber" placeholder="Nhap so can doc">
+    <input type="submit">
+</form>
+<?php
+function NumbertoWord($number)
+{
+    switch ($number) {
+        case 0:
+            return '';
+        case 1:
+            return 'One';
+        case 2:
+            return 'Two';
+        case 3:
+            return 'Three';
+        case 4:
+            return 'Four';
+        case 5:
+            return 'Five';
+        case 6:
+            return 'Six';
+        case 7:
+            return 'Seven';
+        case 8:
+            return 'Eight';
+        case 9:
+            return 'Nine';
+        case 10:
+            return 'Ten';
+        case 11:
+            return 'Eleven';
+        case 12:
+            return 'Twelve';
+        case 13:
+            return 'Thirteen';
+        case 14:
+            return 'Fourteen';
+        case 15:
+            return 'Fifteen';
+        case 16:
+            return 'Sixteen';
+        case 17:
+            return 'Seventeen';
+        case 18:
+            return 'Eighteen';
+        case 19:
+            return 'Nineteen';
+        case 20:
+            return 'Twenty';
+        case 30:
+            return 'Thirty';
+        case 40:
+            return 'Forty';
+        case 50:
+            return 'Fifty';
+        case 60:
+            return 'Sixty';
+        case 70:
+            return 'Seventy';
+        case 80:
+            return 'Eighty';
+        case 90:
+            return 'Ninety';
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $isWord = $_GET['isNumber'];
+    $hundreds = floor($isWord / 100);
+    $tens = floor($isWord / 10) % 10;
+    $units = $isWord % 10;
+    $isWord = $hundreds * 100 + $tens * 10 + $units;
+    $readNumber = '';
+    if ($isWord > 0 && $isWord < 1000) {
+        if (($tens * 10 + $units) >= 0 && ($tens * 10 + $units) <= 20) {
+            if ($hundreds == 0) {
+                $readNumber .= NumbertoWord($tens * 10 + $units);
+            } else {
+                $readNumber = NumbertoWord($hundreds) . ' hundred and ' . NumbertoWord($tens * 10 + $units);
+            }
+        } else {
+            if ($hundreds == 0) {
+                $readNumber = NumbertoWord($tens * 10) . ' ' . NumbertoWord($units);
+            } else {
+                $readNumber = NumbertoWord($hundreds) . ' hundred and ' . NumbertoWord($tens * 10) . ' ' . NumbertoWord($units);
+            }
+        }
+    } elseif ($isWord === 0) {
+        $readNumber = 'Zero';
+    } else {
+        $readNumber = 'out of ability';
+    }
+    echo $readNumber;
+}
+?>
+</body>
+</html>
